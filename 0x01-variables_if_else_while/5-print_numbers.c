6f915db5cf5609c66629432cf4af9fde7cb2a8b0
Esraa6666 <esraanasiry95@gmail.com>
2023-08-12 11:42:37 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int 1;
+	for (in = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf('\n');
+	return (0);
+}
